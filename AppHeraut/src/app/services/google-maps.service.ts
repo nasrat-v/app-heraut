c4a8f15387bee4c045ab9bb273c56d9fb731a52c
Valentin Nasraty <valentin.nasraty@epitech.eu>
2019-11-15 01:34:02 +0100

    ajout map googlemap avec geolocalisation

---
+++ AppHeraut/src/app/services/google-maps.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleMapsService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  // This function makes an http call to google api to decode the cordinates
+  public getAddress(lat: number, lan: number): Observable<any> {
+    return this.http
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lan}&key=${
+          environment.googleMapsApiKey
+        }`
+      )
+      .pipe(
+        map(geoData => {
+          if (!geoData || !geoData.results || geoData.results === 0) {
+            return (null);
+          }
+          return (geoData.results[0].formatted_address);
+        })
+      );
+  }
+
+}
--- AppHeraut/src/app/tab2/tab2.module.ts
+++ AppHeraut/src/app/tab2/tab2.module.ts
@@ -1,16 +1,21 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { IonicModule } from "@ionic/angular";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Tab2Page } from "./tab2.page";
+import { AgmCoreModule } from "@agm/core";
+import { environment } from "../../environments/environment";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: "", component: Tab2Page }]),
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleMapsApiKey
+    })
   ],
   declarations: [Tab2Page]
 })
